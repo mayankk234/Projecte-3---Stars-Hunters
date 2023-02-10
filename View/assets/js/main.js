8cf78b3c9aa006cc2b000e965c3a25730f523e2f
Jordi Palomino i Escarrà <familiajordiescarra@gmail.com>
2023-02-10 19:09:22 +0100

    Comentari puntual

--- View/assets/js/main.js
+++ View/assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("#accedirwebclient").click(function () {
         location.href = "./View/vistaclient.php";
+        //Prova
     });
     $("#accedirwebservidor").click(function () {
         location.href = "./View/vistaservidor.php";
